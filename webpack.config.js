ca0046215826f2772ade1d8e531d90a5df3a8f4c
Kwakcena <rhkrgudwh@gmail.com>
2020-08-03 15:12:11 +0900

    Update webpack.cofig.js and make fake server
    웹팩 설정
    - 루트 디렉토리를 설정하여 나중에 ./components 혹은 ../components 이렇게
      접근해야 하는 디렉토리를 바로 components로 접근하게 합니다.
    - proxy 설정을 통해 webpack-dev-server 실행 시 서버 요청의 프록시를
      설정합니다.
    
    json-server
    - 가짜 서버로 사용할 fake-server 폴더를 만들었습니다.

--- webpack.config.js
+++ webpack.config.js
@@ -12,9 +12,15 @@ module.exports = {
     ],
   },
   resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    // 프로젝트의 루트디렉토리를 설정하여, 나중에 ./components 혹은 ../components
+    // 이렇게 접근해야 되는 디렉토리를 바로 components 로 접근 할 수 있게 해줍니다.
     extensions: ['.js', '.jsx'],
   },
   devServer: {
+    proxy: {
+      '/api': 'http://localhost:3001', // 프록시
+    },
     historyApiFallback: {
       index: 'index.html',
     },
